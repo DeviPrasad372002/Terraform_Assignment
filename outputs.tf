369d5b07b4c0bbc0843c8c5db35d5ada168c4db9
Devi Prasad <145009296+DeviPrasad372002@users.noreply.github.com>
2025-06-23 15:14:59 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,4 @@
 
-output "private_instance_private_ip" {
-  value = module.ec2.private_instance_private_ip
-}
-
-output "bastion_public_ip" {
-  value = module.ec2.bastion_public_ip
-}
-
-
 output "nginx_alb_dns" {
   description = "Public DNS name of the Application Load Balancer"
   value       = module.ec2.nginx_alb_dns
